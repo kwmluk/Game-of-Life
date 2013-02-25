d3fff113e65e092ea0e0e068a55975f7ebb897e5
Michael Luk <kwmluk@gmail.com>
2013-02-24 23:19:40 -0800

    Update 2
    Filter out row/col toggle inputs for numeric inputs only within
    specific range.

--- Game of Life/MainWindow.xaml.cs
+++ Game of Life/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using Game_of_Life.ViewModel;
 
 
@@ -17,6 +18,40 @@ namespace Game_of_Life
             InitializeComponent();
             
             Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+
+        private void toggle_col_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            int result;
+            //Numeric values only
+            if (!(int.TryParse(e.Text, out result)))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                //Stop user from entering values out of range
+                int.TryParse(toggle_col.Text + e.Text, out result);
+
+                if (result >= MainViewModel.getBoardWidth()) e.Handled = true;
+            }
+        }
+
+        private void toggle_row_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            int result;
+            //Numeric values only
+            if (!(int.TryParse(e.Text, out result)))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                //Stop user from entering values out of range
+                int.TryParse(toggle_row.Text + e.Text, out result);
+
+                if (result >= MainViewModel.getBoardHeight()) e.Handled = true;
+            }
         }
     }
 }
